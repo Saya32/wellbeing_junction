46d097e28b89c18bda5b35832a6e90ef66792bb6
Saya32 <sayakabhandari@gmail.com>
2023-11-30 22:15:37 +0000

    Add logo with textfields

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ samples, guidance on mobile development, and a full API reference.
 
 Used https://suragch.medium.com/how-to-include-images-in-your-flutter-app-863889fc0b29 to add images in the app
 Used https://www.youtube.com/watch?v=yJ7qQxzkh9E&t=223s&ab_channel=MitchKoko to get help with firbase
+Used https://www.educative.io/answers/how-to-make-text-bold-in-flutter to learn textstyle in flutter
+Inspiration https://www.youtube.com/watch?v=qI_7znKKlhA&ab_channel=FlutterTeacher for font style
+Free logo design: https://logo-maker.freelogodesign.org/en/logo/edit/038a8833fdd747ad8b8391ae42ec1acc?template=1e164b9c0f4f4ff2aaece49dacd9c695&category=26&companyName=Wellbeing%20Junction
+
+
+
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -11,8 +11,53 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      body: Image.asset('assets/images/logo.png'),
-    );
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        body: SafeArea(
+            child: Center(
+          child: Column(children: [
+            // Logo
+            const SizedBox(height: 30), //allows us to bring down our logo
+            Image.asset('assets/images/logo.png', width: 200),
+
+            // Greetings -  wellbeing Junction
+            const SizedBox(height: 15), //allows padding
+            Text(
+              'Welcome to Wellbeing Junction!',
+              style: TextStyle(
+                  color: const Color.fromARGB(255, 211, 128, 226),
+                  fontSize: 30,
+                  fontFamily: 'DancingScript',
+                  fontWeight: FontWeight.w800),
+            ),
+
+            //Email textfield
+            const SizedBox(height: 30), //allows padding
+            TextField(
+              decoration: InputDecoration(enabledBorder: OutlineInputBorder()),
+            ),
+
+            //Password textfield
+            const SizedBox(height: 30), //allows padding
+            TextField(
+              decoration: InputDecoration(enabledBorder: OutlineInputBorder()),
+            ),
+
+            //Forget password?
+            const SizedBox(height: 30), //allows padding
+            TextField(
+              decoration: InputDecoration(enabledBorder: OutlineInputBorder()),
+            ),
+
+            //Log in button
+            const SizedBox(height: 30), //allows padding
+            TextField(
+              decoration: InputDecoration(enabledBorder: OutlineInputBorder()),
+            ),
+
+            //Google authentication
+
+            //Register as new members
+          ]),
+        )));
   }
 }
